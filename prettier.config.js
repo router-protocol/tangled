842e01fa854da264302ad45abd90eb9b45ba653f
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-11 12:55:33 +0530

    chore:  add prettier, husky

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  endOfLine: 'lf',
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  semi: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports'],
+};
