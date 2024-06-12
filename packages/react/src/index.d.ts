22d6cc585bb7c6b0f3300c2d999599e4ef2bfc32
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-12 19:03:47 +0530

    Delete index.d.ts

--- packages/react/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './providers/TangledContext.js';
-export * from './types/index.js';
-export * from './utils/getConnectorIcon.js';
-export * from './utils/isWalletInstalled.js';
-export * from './utils/isWalletInstalledAsync.js';
