e0d1c8ed40d3dc731e9a98bb4c5e3f719cbe2384
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-18 00:28:42 +0530

    chore: cleanup

--- packages/react/src/providers/TangledContext.tsx
+++ packages/react/src/providers/TangledContext.tsx
@@ -1,33 +1,26 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode, createContext, useState } from 'react';
-import { TangledConfig } from '../types/index.js';
+import { SupportedChainsByType, TangledConfig } from '../types/index.js';
 import createChainConfigs from '../utils/createChainConfigs.js';
 import EVMProvider from './EVMProvider.js';
 import { SolanaProvider } from './SolanaProvider.js';
 import TronProvider from './TronProvider.js';
 import WalletsProvider from './WalletsProvider.js';
 
-export const TangledContext = createContext({});
+export const TangledContext = createContext({
+  config: {} as TangledConfig,
+  chains: {} as SupportedChainsByType,
+});
 
 const queryClient = new QueryClient();
 
-export const TangledContextProvider = ({
-  children,
-  // chains,
-  // evmConnectors,
-  config,
-}: {
-  children: ReactNode;
-  config: TangledConfig;
-  // chains: ChainConfig[];
-  // evmConnectors: CreateConnectorFn[];
-}) => {
+export const TangledContextProvider = ({ children, config }: { children: ReactNode; config: TangledConfig }) => {
   const [chains] = useState(() => {
     return createChainConfigs(config.chains, config.chainConfigs);
   });
 
   return (
-    <TangledContext.Provider value={{}}>
+    <TangledContext.Provider value={{ config, chains }}>
       <QueryClientProvider client={queryClient}>
         <TronProvider>
           <SolanaProvider network={chains.solana[0]}>
