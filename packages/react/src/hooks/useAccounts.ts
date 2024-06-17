6a6c139bbcaff2cb035c3560abfe375f4a399ddc
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-18 02:22:36 +0530

    fix: undefined type

--- packages/react/src/hooks/useAccounts.ts
+++ packages/react/src/hooks/useAccounts.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { useWalletsStore } from '../store/Wallet.js';
 import { ChainType } from '../types/index.js';
+import { Account } from '../types/wallet.js';
 
 /**
  * A hook that returns an array of connected accounts for a {@link ChainType}.
@@ -8,14 +9,14 @@ import { ChainType } from '../types/index.js';
  * @param type The type of chain to return
  * @returns An array of `Account[]`
  */
-export const useAccounts = (type?: ChainType) => {
+export const useAccounts = (type?: ChainType): Account[] => {
   const connectedAccounts = useWalletsStore((store) => store.connectedAccounts);
 
   return useMemo(() => {
     if (type) {
-      return Object.values(connectedAccounts).filter((account) => account?.chainType === type);
+      return Object.values(connectedAccounts).filter((account) => account?.chainType === type) as Account[];
     }
 
-    return Object.values(connectedAccounts).filter((a) => a !== undefined);
+    return Object.values(connectedAccounts).filter((a) => a !== undefined) as Account[];
   }, [connectedAccounts, type]);
 };
