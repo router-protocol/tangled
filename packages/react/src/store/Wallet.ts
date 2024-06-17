104d10924ab3f836fdf54fb48d2a20c591af53c0
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-17 23:46:31 +0530

    chore: add devtool middleware

--- packages/react/src/store/Wallet.ts
+++ packages/react/src/store/Wallet.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 import { Account, ConnectedAccounts, ConnectedWallets, CurrentWallet } from '../types/wallet.js';
 
 interface WalletState {
@@ -15,32 +15,34 @@ interface WalletState {
 }
 
 export const useWalletsStore = create<WalletState>()(
-  persist(
-    (set) => ({
-      currentWallet: { id: '', type: 'evm' },
-      currentAccount: { address: '', chainId: '', chainType: 'evm', wallet: '' },
-      connectedWallets: {
-        evm: {},
-        aleph_zero: {},
-        bitcoin: {},
-        casper: {},
-        cosmos: {},
-        near: {},
-        solana: {},
-        sui: {},
-        tron: {},
-      },
-      connectedAccounts: {},
+  devtools(
+    persist(
+      (set) => ({
+        currentWallet: { id: '', type: 'evm' },
+        currentAccount: { address: '', chainId: '', chainType: 'evm', wallet: '' },
+        connectedWallets: {
+          evm: {},
+          aleph_zero: {},
+          bitcoin: {},
+          casper: {},
+          cosmos: {},
+          near: {},
+          solana: {},
+          sui: {},
+          tron: {},
+        },
+        connectedAccounts: {},
 
-      setCurrentWallet: (wallet) => set(() => ({ currentWallet: wallet })),
-      setCurrentAccount: (account) => set(() => ({ currentAccount: account })),
-      setConnectedWallets: (wallets) =>
-        set((state) => ({ connectedWallets: { ...state.connectedWallets, ...wallets } })),
-      setConnectedAccounts: (accounts) => set(() => ({ connectedAccounts: accounts })),
-    }),
-    {
-      name: 'tangled-wallets', // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-    },
+        setCurrentWallet: (wallet) => set(() => ({ currentWallet: wallet })),
+        setCurrentAccount: (account) => set(() => ({ currentAccount: account })),
+        setConnectedWallets: (wallets) =>
+          set((state) => ({ connectedWallets: { ...state.connectedWallets, ...wallets } })),
+        setConnectedAccounts: (accounts) => set(() => ({ connectedAccounts: accounts })),
+      }),
+      {
+        name: 'tangled-wallets', // name of the item in the storage (must be unique)
+        storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+      },
+    ),
   ),
 );
