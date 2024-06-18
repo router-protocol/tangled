ac61e3f4dd3773c1bd81ad7ac866c3bc9e5b2235
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-06-18 22:28:13 +0530

    feat: useWallets hook

--- packages/react/src/hooks/useWallets.ts
+++ packages/react/src/hooks/useWallets.ts
@@ -1,8 +1,29 @@
-import { useWalletsStore } from '../store/Wallet.js';
+import { Adapter as SolanaAdapter } from '@solana/wallet-adapter-base';
+import { useWallet as useSolanaWallet } from '@solana/wallet-adapter-react';
+import { Adapter as TronAdapter } from '@tronweb3/tronwallet-abstract-adapter';
+import { Mutable } from '@wagmi/core/internal';
+import { Connector as EVMConnector, useConnectors as useEVMConnectors } from 'wagmi';
+import { useTronStore } from './useTronStore.js';
 
-const useWallets = () => {
-  const connectedAccounts = useWalletsStore((store) => store.connectedWallets);
-  return connectedAccounts;
+type Wallets = {
+  evm: Mutable<EVMConnector>[];
+  solana: SolanaAdapter[];
+  tron: TronAdapter[];
 };
 
-export default useWallets;
+/**
+ * A hook that returns the connectors for a given chain type.
+ * If no type is provided, it returns all connectors.
+ * @returns An array of supported connectors
+ */
+export const useWallets = (): Wallets => {
+  const evmConnectors = useEVMConnectors();
+  const { wallets: solanaWallets } = useSolanaWallet();
+  const tronConnectors = useTronStore((state) => state.connectors);
+
+  return {
+    evm: evmConnectors as Mutable<EVMConnector>[],
+    solana: solanaWallets.map((w) => w.adapter),
+    tron: Object.values(tronConnectors).map((c) => c.adapter),
+  };
+};
