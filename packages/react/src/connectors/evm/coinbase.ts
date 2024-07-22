b06f470805d346bc855ecfc6d82c252385a5ecb3
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-07-22 14:04:04 +0530

    chore: eslint fix

--- packages/react/src/connectors/evm/coinbase.ts
+++ packages/react/src/connectors/evm/coinbase.ts
@@ -2,4 +2,5 @@ import type { CoinbaseWalletParameters } from '@wagmi/connectors';
 import { coinbaseWallet } from '@wagmi/connectors';
 
 export const createCoinbaseConnector = /*@__PURE__*/ (params: CoinbaseWalletParameters) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   coinbaseWallet(params) as any;
