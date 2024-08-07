04ac36a6fdb56c730d6e2dcbfddbc8d8a5622c3f
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-07 22:04:27 +0530

    Delete getTronNetwork.ts

--- packages/react/src/utils/getTronNetwork.ts
+++
@@ -1,9 +0,0 @@
-import { NetworkType } from '@tronweb3/tronwallet-abstract-adapter';
-import { ChainId } from '../types/index.js';
-
-export const getTronNetwork = (network: NetworkType | undefined): ChainId | undefined => {
-  if (network === NetworkType.Mainnet) return 'tron-mainnet';
-  // if (network === NetworkType.Shasta) return 'tron-shasta';
-  // if (network === NetworkType.Nile) return 'tron-nile';
-  return undefined;
-};
