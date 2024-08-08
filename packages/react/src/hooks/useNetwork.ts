fc9b062ca5a15ba7a32182c13815e3c274448d1d
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-08 15:34:55 +0530

    feat(useNetwork): switchNetwork mutation

--- packages/react/src/hooks/useNetwork.ts
+++ packages/react/src/hooks/useNetwork.ts
@@ -1,20 +1,59 @@
 import { useMutation } from '@tanstack/react-query';
 
+import { WalletSwitchChainError } from '@tronweb3/tronwallet-abstract-adapter';
 import { useCallback } from 'react';
+import { useSwitchChain } from 'wagmi';
+import { ChainData, ChainId } from '../types/index.js';
+import { WalletInstance } from '../types/wallet.js';
+import { useChains } from './useChains.js';
 import useCurrentAccount from './useCurrentAccount.js';
+import { useWallet } from './useWallet.js';
 
 const useNetwork = () => {
   const currentAccount = useCurrentAccount();
+  const currentWalletInstance = useWallet(currentAccount?.chainType, currentAccount?.wallet);
+  const chains = useChains(currentAccount?.chainType);
 
-  const switchNetwork = useCallback(() => {
-    // switch network
-  }, []);
+  const { switchChainAsync } = useSwitchChain();
+
+  const switchNetwork = useCallback(
+    async (chainId: ChainId): Promise<ChainData | undefined> => {
+      const connector = currentWalletInstance?.connector;
+      if (!connector) {
+        throw new Error('No wallet connector found');
+      }
+
+      const chain = chains.find((chain) => chain.id === chainId);
+
+      if (chain?.type === 'evm') {
+        const switchedChain = await switchChainAsync({
+          chainId: Number(chainId),
+          connector: connector as WalletInstance<'evm'>,
+        });
+
+        return chains.find((chain) => chain.id === switchedChain.id.toString());
+      }
+      if (chain?.type === 'tron') {
+        try {
+          await (connector as WalletInstance<'tron'>).switchChain(chain.tronName);
+        } catch (e) {
+          if (e === WalletSwitchChainError) {
+            console.error('Switch chain is not supported');
+          } else {
+            console.error(e);
+            throw e;
+          }
+        }
+
+        return;
+      }
+    },
+    [chains, currentWalletInstance, switchChainAsync],
+  );
 
   const { mutate, mutateAsync } = useMutation({
     mutationKey: ['switch-network', currentAccount?.wallet],
-    mutationFn: async () => {
-      return switchNetwork();
-    },
+    mutationFn: switchNetwork,
     onError: (error) => {
       console.error(error);
     },
