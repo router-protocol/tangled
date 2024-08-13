c27e5deb755c5e51988d4e7fdb1ac939e3bb3bf0
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-13 12:30:00 +0530

    fix: tronweb default address

--- packages/react/src/utils/tron/getTronweb.ts
+++ packages/react/src/utils/tron/getTronweb.ts
@@ -2,7 +2,9 @@ import { TronWeb } from 'tronweb';
 import { ChainData } from '../../types/index.js';
 
 export const getTronWeb = (chain: ChainData<'tron'>) => {
-  return new TronWeb({
+  const tronweb = new TronWeb({
     fullHost: chain.rpcUrls.fullNode.http[0] ?? '',
   });
+  tronweb.setAddress('TVJ6njG5EpUwJt4N9xjTrqU5za78cgadS2');
+  return tronweb;
 };
