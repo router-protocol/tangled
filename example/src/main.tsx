829a96bb44950e896bb41a773ca5b5d54708a352
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-13 12:29:13 +0530

    chore(example): add tanstack devtools and change package name

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,5 +1,6 @@
 import { TangledContextProvider, solana } from '@tangled3/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
@@ -31,6 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <App />
         </main>
       </TangledContextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
