9d572d71892c80c7930375592478561ce00d4669
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-14 15:00:41 +0530

    fix(example-next): ssr

--- example-next/src/app/page.tsx
+++ example-next/src/app/page.tsx
@@ -1,29 +1,11 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import dynamic from 'next/dynamic';
 import { Example } from '../components/Example';
-
-const TangledContextProvider = dynamic(() => import('@tangled3/react').then((mod) => mod.TangledContextProvider), {
-  ssr: false,
-});
-
-const queryClient = new QueryClient();
 
 export default function HomePage() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TangledContextProvider
-        config={{
-          projectName: 'Tangled Example',
-          chainConfigs: {},
-          projectId: '41980758771052df3f01be0a46f172a5',
-        }}
-      >
-        <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
-          <Example />
-        </main>
-      </TangledContextProvider>
-    </QueryClientProvider>
+    <main className='flex min-h-screen flex-col items-center justify-center text-white'>
+      <Example />
+    </main>
   );
 }
--- example-next/src/components/Example.tsx
+++ example-next/src/components/Example.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   CHAIN_TYPES,
   ConnectedAccount,
