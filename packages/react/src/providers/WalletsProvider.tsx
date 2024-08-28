19b014b8ab868636c27ab9622807e1aad9242cfa
Jayesh Bhole <111138421+jayeshbhole-rp@users.noreply.github.com>
2024-08-28 17:31:30 +0530

    chore: rename var

--- packages/react/src/providers/WalletsProvider.tsx
+++ packages/react/src/providers/WalletsProvider.tsx
@@ -1,6 +1,6 @@
 import { useWallet as useSolanaWallet } from '@tangled3/solana-react';
 import { ReactNode, useEffect } from 'react';
-import { useConnections } from 'wagmi';
+import { useConnections as useEVMConnections } from 'wagmi';
 import { useAlephStore } from '../hooks/useAlephStore.js';
 import { useTangledConfig } from '../hooks/useTangledConfig.js';
 import { useTronStore } from '../hooks/useTronStore.js';
@@ -10,7 +10,7 @@ import { ConnectedAccount, ConnectedWallet } from '../types/wallet.js';
 
 const WalletsProvider = ({ children }: { children: ReactNode }) => {
   const { chains } = useTangledConfig();
-  const evmConnections = useConnections();
+  const evmConnections = useEVMConnections();
   const { connections: solanaWallets, wallet: solConnectedWallet } = useSolanaWallet();
   const tronConnectors = useTronStore((state) => state.connectors);
   const alephConnectors = useAlephStore((state) => state.connectors);
