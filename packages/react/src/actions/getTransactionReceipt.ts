23fde093fa31fa7833b66ea00f906f43e7066517
Ankit Singh <ankit@routerprotocol.com>
2024-09-12 01:14:03 +0530

    feat: transaction reciept added for alephZero

--- packages/react/src/actions/getTransactionReceipt.ts
+++ packages/react/src/actions/getTransactionReceipt.ts
@@ -1,5 +1,6 @@
 import { getTransactionReceipt as getEVMTransactionReceipt } from '@wagmi/core';
 import { ChainData, ChainType, ConnectionOrConfig, TransactionReceipt } from '../types/index.js';
+import { TransactionParams } from './waitForTransaction.js';
 
 export type GetTransactionReceiptOverrides<C extends ChainType = ChainType> = C extends 'solana'
   ? {
@@ -8,7 +9,7 @@ export type GetTransactionReceiptOverrides<C extends ChainType = ChainType> = C 
   : any;
 
 export type GetTransactionReceiptParams<C extends ChainType = ChainType> = {
-  txHash: string;
+  transactionParams: TransactionParams<C>;
   chain: ChainData<C>;
   config: ConnectionOrConfig;
   overrides: GetTransactionReceiptOverrides<C> | undefined;
@@ -16,19 +17,20 @@ export type GetTransactionReceiptParams<C extends ChainType = ChainType> = {
 
 /**
  * Get transaction receipt
- * @param txHash - Transaction hash
+ * @param transactionParams - Transaction Parameters {@link TransactionParams}
  * @param chain - {@link ChainData}
  * @param config {@link ConnectionOrConfig}
  * @returns Transaction Receipt {@link TransactionReceipt}
  */
 export const getTransactionReceipt = async <C extends ChainType>({
-  txHash,
+  transactionParams,
   chain,
   config,
   overrides = {} as GetTransactionReceiptOverrides,
 }: GetTransactionReceiptParams<C>): Promise<TransactionReceipt<C>> => {
   // evm chain
   if (chain.type === 'evm') {
+    const { txHash } = transactionParams as TransactionParams<'evm'>;
     const receipt = await getEVMTransactionReceipt(config.wagmiConfig, {
       hash: txHash as `0x${string}`,
       chainId: chain.id,
@@ -37,10 +39,12 @@ export const getTransactionReceipt = async <C extends ChainType>({
   }
 
   if (chain.type === 'tron') {
+    const { txHash } = transactionParams as TransactionParams<'tron'>;
     return (await config.tronWeb.trx.getTransactionInfo(txHash)) as TransactionReceipt<C>;
   }
 
   if (chain.type === 'solana') {
+    const { txHash } = transactionParams as TransactionParams<'solana'>;
     const result = config.solanaConnection.getTransaction(txHash, {
       maxSupportedTransactionVersion: 0,
       ...overrides,
@@ -49,6 +53,44 @@ export const getTransactionReceipt = async <C extends ChainType>({
       throw new Error('Transaction not found');
     }
     return result as TransactionReceipt<C>;
+  }
+
+  if (chain.type === 'alephZero') {
+    const { blockHash, extrinsicIndex } = transactionParams as TransactionParams<'alephZero'>;
+    const alephZero = config.alephZeroApi;
+
+    const signedBlock = await alephZero.rpc.chain.getBlock(blockHash);
+
+    // Get the specific extrinsic
+    const extrinsic = signedBlock.block.extrinsics[extrinsicIndex];
+
+    if (!extrinsic) {
+      throw new Error('Extrinsic not found');
+    }
+
+    // Get the events for this block
+    const apiAt = await alephZero.at(blockHash);
+    const allRecords = (await apiAt.query.system.events()).toPrimitive() as any[];
+
+    const extrinsicEvents = allRecords.filter(
+      (event) => event.phase.applyExtrinsic && event.phase.applyExtrinsic === extrinsicIndex,
+    );
+
+    const transactionData = {
+      blockHash,
+      extrinsicIndex,
+      extrinsic: extrinsic,
+      extrinsicHash: extrinsic.hash.toHuman(),
+      method: extrinsic.method.toHuman(),
+      args: extrinsic.args.map((arg) => arg.toHuman()),
+      events: extrinsicEvents,
+    };
+
+    if (!transactionData) {
+      throw new Error('Transaction not found');
+    }
+
+    return transactionData as TransactionReceipt<C>;
   }
 
   throw new Error('Chain type not supported');
--- packages/react/src/hooks/useTransactionReceipt.ts
+++ packages/react/src/hooks/useTransactionReceipt.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { GetTransactionReceiptOverrides, getTransactionReceipt } from '../actions/getTransactionReceipt.js';
+import { TransactionParams } from '../actions/waitForTransaction.js';
 import { ChainId } from '../types/index.js';
 import { useChain } from './useChain.js';
 import { useConnectionOrConfig } from './useConnectionOrConfig.js';
 
 export type UseTransactionReceiptParams = {
-  /** Transaction hash */
-  txHash: string | undefined;
+  /** Transaction hash {@link TransactionParams} */
+  transactionParams: TransactionParams;
   /** Chain ID of transaction */
   chainId: ChainId;
   /** Transaction overrides {@link GetTransactionReceiptOverrides} */
@@ -15,17 +16,17 @@ export type UseTransactionReceiptParams = {
 
 /**
  * Transaction Receipt Hook
- * @param txHash - Transaction hash
+ * @param transactionParams - Transaction Parameters
  * @param chainId - Chain ID of transaction
  * @param overrides - Transaction overrides {@link GetTransactionReceiptOverrides}
  * @returns Query object
  */
-export const useTransactionReceipt = ({ txHash, chainId, overrides }: UseTransactionReceiptParams) => {
+export const useTransactionReceipt = ({ transactionParams, chainId, overrides }: UseTransactionReceiptParams) => {
   const connectionOrConfig = useConnectionOrConfig();
   const chain = useChain(chainId);
 
   return useQuery({
-    queryKey: ['transaction receipt', txHash, chainId],
+    queryKey: ['transaction receipt', transactionParams, chainId],
     queryFn: async () => {
       if (!connectionOrConfig) {
         throw new Error('Connections or config not found');
@@ -33,18 +34,18 @@ export const useTransactionReceipt = ({ txHash, chainId, overrides }: UseTransac
       if (!chain) {
         throw new Error('Chain is not supported');
       }
-      if (!txHash) {
+      if (!transactionParams) {
         throw new Error('Transaction hash is required');
       }
 
       return await getTransactionReceipt({
-        txHash,
+        transactionParams,
         chain,
         config: connectionOrConfig,
         overrides,
       });
     },
-    enabled: Boolean(connectionOrConfig && chain && txHash),
+    enabled: Boolean(connectionOrConfig && chain && transactionParams),
     retry: false,
     refetchOnWindowFocus: false,
   });
