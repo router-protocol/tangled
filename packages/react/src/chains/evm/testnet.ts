5ca0ea8b26f725fb88b3e7942a3e8d3fdd4ab605
soumyaRouterP <soumya@routerprotocol.com>
2025-01-22 17:57:52 +0530

    remove: hyperliquid testnet

--- packages/react/src/chains/evm/testnet.ts
+++ packages/react/src/chains/evm/testnet.ts
@@ -13,9 +13,4 @@ export const arbitrumSepolia: EVMChain = { ...vArbitrumSepolia, type: 'evm' } as
 export const avalancheFuji: EVMChain = { ...vAvalancheFuji, type: 'evm' } as const;
 export const amoy: EVMChain = { ...vAmoy, type: 'evm' } as const;
 
-export * from './testnets/beraChain.testnet.js';
-export * from './testnets/firechain.testnet.js';
-export * from './testnets/movement.testnet.js';
-export * from './testnets/oasisSapphire.testnet.js';
-export * from './testnets/pentagon.testnet.js';
-export * from './testnets/soneium.testnet.js';
+export * from './testnets/index.js';
