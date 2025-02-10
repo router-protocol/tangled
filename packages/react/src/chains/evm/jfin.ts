b67a8bd09834740602e097e72e937a91f811872c
Laukik verma <laukikverma03@gmail.com>
2025-02-11 02:11:27 +0530

    add/jfin chain

---
+++ packages/react/src/chains/evm/jfin.ts
@@ -0,0 +1,29 @@
+import { EVMChain } from '../../types/index.js';
+
+export const jfin: EVMChain = {
+  id: 3501,
+  name: 'Jfin',
+  type: 'evm',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'JFIN',
+    symbol: 'JFIN',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.jfinchain.com'],
+      webSocket: ['https://rpc.jfinchain.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Jfin Explorer',
+      url: 'https://exp.jfinchain.com',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+    },
+  },
+};
